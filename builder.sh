6aef426b1d5879f1b49d767ce65631b460099241
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2025-01-22 15:02:26 +0800

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -12,12 +12,12 @@ case $ARCH in
 esac
 wget https://github.com/heiher/natmap/releases/latest/download/natmap-linux-$DL -O /files/natmap
 
+# H@H 客户端版本更新后，需手动确认依赖是否变更
 DEPS=java.base
 
-# ppc64le 与 s390x 可安装 openjdk11
-# ppc64le 目前版本 qemu 下执行 java 报错
-# s390x 提示 "Ambiguous z/Architecture detection!"
-# 问题解决前，可能需要在发行镜像上安装 openjdk8-jre-base
+# x86 armhf armv7 不支持 jlink
+# ppc64le 与 s390x 可安装 openjdk11，ppc64le 目前版本 qemu 下执行 java 报错，s390x 提示 "Ambiguous z/Architecture detection!"
+# 可能需要在发行镜像上安装 openjdk8-jre-base
 [[ $ARCH =~ 'x86_64|aarch64|ppc64le|s390x' ]] && \
 apk add openjdk11 && \
 jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules $DEPS --output /files/jre

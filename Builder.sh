f1aa97d26c694ebd297ecf32d6e2df4cffc079fe
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2025-01-22 12:36:47 +0800

    Create Builder.sh

---
+++ Builder.sh
@@ -0,0 +1,28 @@
+mkdir -p /files
+
+ARCH=$(cat etc/apk/arch)
+case $ARCH in
+  x86) DL=i586;;
+  x86_64) DL=x86_64;;
+  armhf) DL=arm32hf;;
+  armv7) DL=arm32v7;;
+  aarch64) DL=arm64;;
+  ppc64le) DL=powerpc64;;
+  riscv64) DL=riscv64;;
+  s390x) DL=s390x;;
+esac
+wget https://github.com/heiher/natmap/releases/latest/download/natmap-linux-$DL -O /files/natmap
+
+wget https://repo.e-hentai.org/hath/HentaiAtHome_1.6.4.zip -O hath.zip
+apk add unzip
+unzip hath.zip HentaiAtHome.jar -d /files
+
+[[ $ARCH =~ 'x86_64|aarch64|ppc64le|s390x' ]] && \
+apk add openjdk11 && \
+DEPS=$(jdeps /files/HentaiAtHome.jar | awk '{print$NF}' | uniq) && \
+jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules $(echo $DEPS | tr ' ' ,) --output /files/jre
+
+[[ $ARCH =~ 'riscv64' ]] && \
+apk add openjdk21 binutils && \
+DEPS=$(jdeps /files/HentaiAtHome.jar | awk '{print$NF}' | uniq) && \
+jlink --no-header-files --no-man-pages --compress=zip-9 --strip-debug --add-modules $(echo $DEPS | tr ' ' ,) --output /files/jre

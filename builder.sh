3639c3a18c5f74978ce608806c3620e6ab75319b
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2025-01-22 13:40:24 +0800

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,3 +1,4 @@
+# 下载 NATMap，识别对应的指令集架构
 ARCH=$(cat etc/apk/arch)
 case $ARCH in
   x86) DL=i586;;
@@ -11,11 +12,14 @@ case $ARCH in
 esac
 wget https://github.com/heiher/natmap/releases/latest/download/natmap-linux-$DL -O /files/natmap
 
+# ppc64le 可安装 openjdk11，但目前版本 qemu 下执行 jlink 报错
+# 在问题修复前，于发行镜像上安装 openjdk8-jre-base
 [[ $ARCH =~ 'x86_64|aarch64|s390x' ]] && \
 apk add openjdk11 && \
 DEPS=$(jdeps /files/HentaiAtHome.jar | awk '{print$NF}' | uniq) && \
 jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules $(echo $DEPS | tr ' ' ',') --output /files/jre
 
+# riscv64 不支持 openjdk21 以前的版本
 [[ $ARCH =~ 'riscv64' ]] && \
 apk add openjdk21 binutils && \
 DEPS=$(jdeps /files/HentaiAtHome.jar | awk '{print$NF}' | uniq) && \

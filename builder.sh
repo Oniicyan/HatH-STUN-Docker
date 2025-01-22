ffe5d38dd2779fd6aa91c2c0162a544fb60ea6ba
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2025-01-22 14:19:19 +0800

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -12,17 +12,17 @@ case $ARCH in
 esac
 wget https://github.com/heiher/natmap/releases/latest/download/natmap-linux-$DL -O /files/natmap
 
+DEPS=java.base
+
 # ppc64le 可安装 openjdk11，但目前版本 qemu 下执行 jlink 报错
 # 问题解决前，在发行镜像上安装 openjdk8-jre-base
-[[ $ARCH =~ 'x86_64|aarch64|s390x' ]] && \
+[[ $ARCH =~ 'x86_64|aarch64|ppc64le|s390x' ]] && \
 apk add openjdk11 && \
-DEPS=$(jdeps /files/HentaiAtHome.jar | awk '{print$NF}' | uniq) && \
-jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules $(echo $DEPS | tr ' ' ',') --output /files/jre
+jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules $DEPS --output /files/jre
 
 # riscv64 不支持 openjdk21 以前的版本
 [[ $ARCH =~ 'riscv64' ]] && \
 apk add openjdk21 binutils && \
-DEPS=$(jdeps /files/HentaiAtHome.jar | awk '{print$NF}' | uniq) && \
-jlink --no-header-files --no-man-pages --compress=zip-9 --strip-debug --add-modules $(echo $DEPS | tr ' ' ',') --output /files/jre
+jlink --no-header-files --no-man-pages --compress=zip-9 --strip-debug --add-modules $DEPS --output /files/jre
 
-exit 0
+# exit 0
